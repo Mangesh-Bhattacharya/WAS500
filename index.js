4311c98e5f07a4c9b22ef95e80f80a589c86d2ab
Mangesh-Bhattacharya <codeoriginals.20@gmail.com>
2022-11-09 16:01:28 -0500

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // connect to localhost:3000
 const http = require('http');
 const hostname = 'localhost'
+const fs = require('fs');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    })
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    });
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, function (error) {
+    if (error) {
+        console.log('Error: ', error);
+    } else {
+        console.log('Server running on port: ' + port);
+    }
 });
